35f437a3c11c154a59c122a4ded9a1c10e37be01
doolybom1 <elqkqkwkd@gmail.com>
2020-02-20 16:12:18 +0900

    add

---
+++ emsdb-tbl_movie.sql
@@ -0,0 +1,8 @@
+USE emsdb;
+CREATE TABLE tbl_movie(
+	id BIGINT PRIMARY KEY AUTO_INCREMENT,
+    m_rank int UNIQUE,
+    m_title VARCHAR(125),
+    m_detail_url VARCHAR(255),
+    m_image_url VARCHAR(255)
+);
